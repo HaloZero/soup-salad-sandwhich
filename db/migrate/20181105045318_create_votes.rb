08dfc0fb10a13ef0c8b0525243175677d268e961
rohan <rohan.dhaimade@gmail.com>
2018-11-04 21:55:27 -0800

    Models

---
+++ db/migrate/20181105045318_create_votes.rb
@@ -0,0 +1,10 @@
+class CreateVotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :votes do |t|
+      t.references :photo, foreign_key: true
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
