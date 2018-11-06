14d3772ebfc6613886d466a3e19ffd8cf2e56b94
rohan <rohan.dhaimade@gmail.com>
2018-11-05 20:22:41 -0800

    result table

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -16,6 +16,25 @@ class VoteController < ApplicationController
 		end
 	end
 
+	def results()
+		votes = Vote.all.includes(:photo)
+		photos = votes.map { |v| v.photo }.uniq.sort
+
+		results = []
+		photos.each do |photo| 
+			vote_for_photo = votes.select { |v| v.photo_id = photo.id }
+			vote_json = {
+				photo: photo.name,
+				photo_url: photo.url,
+				soup: vote_for_photo.select { |v| v.food_type == 'soup' }.count,
+				sandwhich: vote_for_photo.select { |v| v.food_type == 'sandwhich' }.count,
+				salad: vote_for_photo.select { |v| v.food_type == 'salad' }.count,
+			}
+			results.append(vote_json)
+		end
+		@results = results
+	end
+
 	def find_photo_to_vote(session_id)
 		voted_photos = Vote.all.map { |x| x.photo_id }
 		photo = Photo.where.not(id: voted_photos).shuffle.first
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   	root 'vote#index'
  	resources :vote
+ 	get '/results', to: 'vote#results'
 
 end
