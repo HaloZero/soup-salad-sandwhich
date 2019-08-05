416c3cbebf2ba6084858c2957dd5b3014bfc7188
rohan <rohan.dhaimade@gmail.com>
2019-08-04 19:07:46 -0700

    Expanding food list

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,14 @@
 class Food
 	def self.food_list
-		['spaghetti', 'blt', 'cinnamon_churro', 'ramen', 'chicken_wings', 'muffin', 'ice_cream']
+		[
+			'spaghetti', 'blt', 'cinnamon_churro', 'ramen', 'chicken_wings', 
+			'muffin', 'ice_cream', 'popcorn', 'poutine', 'chicken_taco', 
+			'chicken_parmesan', 'lasagna', 'poke_bowl', 'crossaint', 'chicken_arepa',
+			'donut', 'peking_duck', 'crab_roll', 'pepperoni_pizza', 'apple_pie',
+			'tater_tots', 'baked_potato', 'nachos', 'fried_chicken', 'clam_chowder',
+			'macaroni_and_cheese', 'fortune_cookie', 'hot_dog', 'chocolate_chip_cookie',
+			'bananas_foster', 'shish_kebab', 'chicken_tikka_masala', 'steak_burrito',
+			'souffle', 'spam_masubi', 'soup_dumplings', 'morning_bun', 'fruit_loops'
+		]
 	end
 end
